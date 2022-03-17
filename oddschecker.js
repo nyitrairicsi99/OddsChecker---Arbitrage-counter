a200cd36e22ee9afab5e0100a2a17a5ec0cf92d7
nyitrairicsi99 <58612330+nyitrairicsi99@users.noreply.github.com>
2022-03-17 20:07:06 +0100

    Revert "hide disabled sites"
    This reverts commit 36dfeea0964a356c7c5ecd0223321ed97fbc9947.

--- oddschecker.js
+++ oddschecker.js
@@ -124,7 +124,6 @@
             } else {
                 buttons[i].style.color = "red"
                 buttons[i].style.fontWeight = "100";
-                buttons[i].style.opacity = "0";
             }
         }
         maxElement.style.fontWeight = "900";
@@ -183,7 +182,6 @@
             } else {
                 buttons[i].style.color = "red"
                 buttons[i].style.fontWeight = "100";
-                buttons[i].style.opacity = "0";
             }
         }
         maxElement.style.fontWeight = "900";
